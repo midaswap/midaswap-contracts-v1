3f61da987141ac8762d81f7a65632c69098f8e83
Laubswind <53655625+Laubswind@users.noreply.github.com>
2023-05-05 13:08:35 +0800

    Update Constants.sol

--- libraries/Constants.sol
+++ libraries/Constants.sol
@@ -8,10 +8,8 @@ pragma solidity 0.8.10;
 library Constants {
     uint256 internal constant SCALE_OFFSET = 128;
     uint256 internal constant SCALE = 1 << SCALE_OFFSET;
-
-    uint256 internal constant PRECISION = 1e18;
+    // uint256 internal constant PRECISION = 1e18;
     uint256 internal constant BASIS_POINT_MAX = 10_000;
     int256 internal constant REAL_ID_SHIFT = 1 << 23;
-
     uint256 internal constant Bin_Step_Value = SCALE + SCALE / BASIS_POINT_MAX;
 }
