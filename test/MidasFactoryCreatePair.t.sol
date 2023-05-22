a2b131d760b0d4975194c4f415f6e506dfa84fc5
Joseph Xu <45804218+crossjay@users.noreply.github.com>
2023-05-22 16:30:53 +0800

    ADD TEST FILE

---
+++ test/MidasFactoryCreatePair.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {IMidasFactory721} from "../interfaces/IMidasFactory721.sol";
+
+contract MidasFactoryCreatePair {
+
+    IMidasFactory721 public factory;
+
+    constructor (address _factory) {
+        factory = IMidasFactory721(_factory);
+    }
+    
+    function createPairWithValidAddress(
+        address _token0,
+        address _token1
+    ) external returns (address lpToken, address pair) {
+        (lpToken, pair) = factory.createERC721Pair(_token0, _token1);
+    }
+
+    function createPairTwice(
+        address _token0,
+        address _token1
+    ) external returns (address lpToken, address pair) {
+        (lpToken, pair) = factory.createERC721Pair(_token0, _token1);
+        (lpToken, pair) = factory.createERC721Pair(_token0, _token1);
+    }
+    
+}
