1173c0e4fbd4ec5153c9d9d3420668d5c9b7fced
Joseph Xu <45804218+0xjosephxu@users.noreply.github.com>
2023-10-13 06:43:01 -0400

    feat: new native limit orders

--- interfaces/IMidasRouter.sol
+++ interfaces/IMidasRouter.sol
@@ -104,6 +104,13 @@ interface IMidasRouter {
         uint256 _deadline
     ) external returns (uint128[] memory lpTokenIds);
 
+    function openMultiLimitBuyOrdersETH(
+        address _tokenX,
+        address _tokenY,
+        uint24[] calldata _ids,
+        uint256 _deadline
+    ) external payable returns (uint128[] memory lpTokenIds);
+
     function claimFee(
         address _tokenX,
         address _tokenY,
